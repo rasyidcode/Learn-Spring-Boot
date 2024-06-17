633c8223944b64ceef2db4708b5e24f084a336ac
rasyidcode <rasyidcode@gmail.com>
2024-06-17 23:20:51 +0700

    add starting spring-petclinic-clone project

---
+++ spring-petclinic-clone/src/main/java/com/rasyidcode/spring_petclinic_clone/SpringPetclinicCloneApplication.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.spring_petclinic_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetclinicCloneApplication.class, args);
+	}
+
+}
---
+++ spring-petclinic-clone/src/test/java/com/rasyidcode/spring_petclinic_clone/SpringPetclinicCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.spring_petclinic_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
