1d76e1274971ed7f53297562603c0e0862ce6e92
rasyidcode <rasyidcode@gmail.com>
2024-06-17 21:16:18 +0700

    add more

---
+++ quoters/src/main/java/com/rasyidcode/quoters/QuotersApplication.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.quoters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotersApplication.class, args);
+	}
+
+}
---
+++ quoters/src/test/java/com/rasyidcode/quoters/QuotersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.quoters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
