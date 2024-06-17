b1209cffe3d870dc3b31c2145ef1017a06d8aa4e
rasyidcode <rasyidcode@gmail.com>
2024-06-17 22:51:52 +0700

    add gs-rest-hateoas

---
+++ gs-rest-hateoas/src/main/java/com/rasyidcode/gs_rest_hateoas/GsRestHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.gs_rest_hateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsRestHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsRestHateoasApplication.class, args);
+	}
+
+}
---
+++ gs-rest-hateoas/src/test/java/com/rasyidcode/gs_rest_hateoas/GsRestHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.gs_rest_hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsRestHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
