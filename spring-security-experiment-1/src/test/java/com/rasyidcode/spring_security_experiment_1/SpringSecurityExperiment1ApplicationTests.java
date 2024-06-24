16734e504f5f21b8f3e125581b39f360c5333197
NB18 <NB18@DESKTOP-9LS0JGV>
2024-06-24 17:29:55 +0700

    add spring security tutorial

---
+++ spring-security-experiment-1/src/test/java/com/rasyidcode/spring_security_experiment_1/SpringSecurityExperiment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rasyidcode.spring_security_experiment_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExperiment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
